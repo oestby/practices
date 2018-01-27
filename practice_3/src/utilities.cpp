2c1ecb58773df3d00db40f488e11b91ab67a91bd
Oskar Østby <oskar.oestby@gmail.com>
2018-01-27 16:06:05 +0100

    THIS fixes the error

--- practice_3/src/utilities.cpp
+++ practice_3/src/utilities.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 
 int randomWithLimits(int lower, int upper){
-    int num = std::rand() % (upper-lower) + lower;
+    int num = std::rand() % (upper-lower+1) + lower;
     return num;
 }
 
