e86876287bf587a9f31161c935297d26afe9d1ea
Oskar Østby <oskar.oestby@gmail.com>
2018-01-27 16:04:22 +0100

    Fixes an error with the random number generation

--- practice_3/src/utilities.cpp
+++ practice_3/src/utilities.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 
 int randomWithLimits(int lower, int upper){
-    int num = std::rand() % upper + lower;
+    int num = std::rand() % (upper-lower) + lower;
     return num;
 }
 
