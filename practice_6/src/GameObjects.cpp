60baae9d3919cc6785e39586fa948d9b19a9395f
Oskiboy <oskar.oestby@gmail.com>
2018-02-08 22:49:04 +0100

    Adds implementation of target and some cannon.

--- practice_6/src/GameObjects.cpp
+++ practice_6/src/GameObjects.cpp
@@ -25,3 +25,43 @@ double Cannon::getTipX() const {
 double Cannon::getTipY() const {
     return length*sin(degToRad(theta));
 }
+
+Target::Target(double size, double position):
+size(size), position(position)
+{
+    this->shape = sf::RectangleShape(sf::Vector2f(this->size, this->size));
+    this->shape.setFillColor(sf::Color::Red);
+    this->shape.setPosition(this->position, this->position);
+}
+
+void
+Target::update()
+{
+
+}
+
+void
+Target::draw(sf::RenderWindow& window)
+{
+    window.draw(this->shape);
+}
+
+
+Cannon::Cannon()
+{
+    this->shape.setOrigin(this->width/2, 0);
+    this->shape.setPosition(0,0);
+}
+
+void
+Cannon::update()
+{
+    this->shape.setRotation(this->theta);
+}
+
+void
+Cannon::draw(sf::RenderWindow& window)
+{
+    window.draw(this->shape);
+}
+
