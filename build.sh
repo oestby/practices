d08d7b1819b7b390fd65a19bf5f32b91443046a0
Oskar Østby <oskar.oestby@gmail.com>
2018-03-12 16:07:20 +0100

    Edits folder structure and yml file.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set e-
+cd practice_1
+make
+cd ..
+cd practice_2
+make
+cd ..
+cd practice_3
+make
+cd ..
+cd practice_4
+make
+cd ..
+cd practice_5
+make
+cd ..
+cd practice_6
+make
+cd ..
+cd practice_7
+make
+cd ..
+cd practice_8
+make
+cd ..
+cd practice_9
+make
+cd ..
+cd practice_10
+make
+cd ..
