bc30e82e660c1bfffded305f2e2c23557128ccc3
Oskar Østby <oskar.oestby@gmail.com>
2018-03-12 20:05:16 +0100

    Should fix build

--- build.sh
+++ build.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
-set e-
-cd practice_1
-make
-cd ..
-cd practice_2
-make
-cd ..
-cd practice_3
-make
-cd ..
-cd practice_4
-make
-cd ..
-cd practice_5
-make
-cd ..
-cd practice_6
-make
-cd ..
-cd practice_7
-make
-cd ..
-cd practice_8
-make
-cd ..
-cd practice_9
-make
-cd ..
-cd practice_10
-make
-cd ..
+for dir in */;
+do
+    dir=${dir%*/}
+    ./${dir##*/}/build.sh
+done
